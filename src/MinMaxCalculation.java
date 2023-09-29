0b3a1918c5cab6dfafef3e26e0e27a5c2781302e
Mattis Bodynek <mattisbodynek@gmail.com>
2023-09-29 13:16:09 -0400

    Added method to compute max

--- src/MinMaxCalculation.java
+++ src/MinMaxCalculation.java
@@ -1,3 +1,15 @@
 public class MinMaxCalculation {
-
+    /**
+     * @author: Mattis
+     *          Compute the max of an array of numbers.
+     */
+    public static double max(double... numbers) {
+        double max = Double.NEGATIVE_INFINITY;
+        for (double num : numbers) {
+            if (max < num) {
+                max = num;
+            }
+        }
+        return max;
+    }
 }
